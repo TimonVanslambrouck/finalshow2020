820ed195ee1f8c86b5d3766e64ac4c88f50dac7c
Joppe Rabijns <joppe.rabijns@student.ehb.be>
2021-06-14 20:57:58 +0200

    scroll test

--- finalshow/src/app/app.component.ts
+++ finalshow/src/app/app.component.ts
@@ -6,6 +6,8 @@ import * as ORBIT from 'three/examples/jsm/controls/OrbitControls';
 import { ModelLoaderService } from './model-loader.service';
 import { GuiService } from './gui.service';
 import { SkyComponent } from './sky/sky.component';
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
 
 @Component({
   selector: 'app-root',
@@ -19,26 +21,47 @@ export class AppComponent {
   renderer = new THREE.WebGLRenderer();
   modelLoader=new ModelLoaderService();
   guiService=new GuiService();
-  sky=new SkyComponent();
+  sky = new SkyComponent();
   //rectLight=new THREE.RectAreaLight(0xffffff,50,200,200);
   //hemiLight=new THREE.HemisphereLight( 0xeeeeee, 0xeeeeee, 1 );
   //hemiLight = new THREE.HemisphereLight( 0xffffff, 0x444444, 1 );
   hemiLight = new THREE.HemisphereLight( 0xffffff, 0xffffff, 1 );
   gui=new dat.GUI();
-  orbit=new ORBIT.OrbitControls(this.camera,this.renderer.domElement);
+ // orbit=new ORBIT.OrbitControls(this.camera,this.renderer.domElement);
 
   guiSettings(){
     this.guiService.position("camera",this.camera,true);
   }
 
   controls(){
-    console.log(this.orbit);
+   // console.log(this.orbit);
     //this.orbit.enableZoom=false;
   }
 
-  sceneSettings(){
-    //this.scene.background=new THREE.Color();
-  }
+  scrollSettings(){  
+    gsap.registerPlugin(ScrollTrigger);
+
+    var cam_anim = gsap.timeline({
+      scrollTrigger: {
+        trigger: 'canvas',
+        scrub: 1.2,
+        start: 'top top',
+        end:'+=5000',
+        markers: true,
+      }
+    }).to(this.camera.position, {
+      x: 200,
+      y: 50,
+      z: 300,
+      duration: 1,
+      ease: 'none'
+    }).to(this.camera.position, {
+      y: 200,
+      duration: 1,
+      ease: 'none'
+    });
+    
+  };
 
   light(){
     //this.rectLight.position.set( 5, 100, 0 );
@@ -64,16 +87,14 @@ export class AppComponent {
 }
 
 ngOnInit(): void {
-  this.modelLoader.loadModel(this.scene,'../assets/3D_models/cloud/scene.gltf',"cloud");
   this.modelLoader.loadModel(this.scene,'../assets/3D_models/north_american_x-15/scene.gltf',"x-15");
   this.modelLoader.initTerrain(this.scene,'../assets/Terrain/jotunheimen.bin','../assets/Terrain/jotunheimen-texture-altered.jpg',new THREE.PlaneGeometry(60, 60, 199, 199));
   this.sky.skyGui(this.scene);
   this.controls();
   this.guiSettings();
-  this.sceneSettings();
+  this.scrollSettings();
   this.light();
   this.render();
   this.animate();
 }
-
 }
