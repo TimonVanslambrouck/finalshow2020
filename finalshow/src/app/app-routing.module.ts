eab3b9c250b5f5201367cc62a5a93edf8e7465af
Lorenz Reweghs <lorenz.reweghs@student.ehb.be>
2021-06-21 10:18:21 +0200

    mobile routing added

--- finalshow/src/app/app-routing.module.ts
+++ finalshow/src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HubComponent } from 'src/hub/hub.component';
 import { AnimationComponent } from 'src/animation/animation.component';
+import { MobileComponent } from 'src/mobile/mobile.component'; 
 
 const routes: Routes = [
   { path: '', component: AnimationComponent },
   { path: 'hub', component: HubComponent },
+  { path: 'mobile', component: MobileComponent },
 ];
 
 @NgModule({
