0c23fa7d938f5e914f77acb8e9595008ebe8e205
Timon Vanslambrouck <timon.vanslambrouck@student.ehb.be>
2021-06-24 17:08:41 +0200

    removed unnecessary code + added drone link

--- finalshow/src/hub/poi/poi.component.ts
+++ finalshow/src/hub/poi/poi.component.ts
@@ -33,7 +33,6 @@ export class PoiComponent implements OnInit {
       if(intersect.object.type ==='Sprite'){
       }		
       if(intersect.object.name ==="FAQ"){
-        //window.open("https://www.erasmushogeschool.be/nl/faq");
         popupComponent.showPopup(renderer,cssrenderer,controls,intersect.object.name);
       }
       if(intersect.object.name ==="Bureau"){
@@ -43,15 +42,13 @@ export class PoiComponent implements OnInit {
         window.open("https://seads.network/");
       }
       if(intersect.object.name ==="Drone"){
-        //window.open("https://www.erasmushogeschool.be/nl/faq");
-        popupComponent.showPopup(renderer,cssrenderer,controls,intersect.object.name);
+        window.location.href = '/';
       }
       if(intersect.object.name ==="Showcase"){
         animationLaunch = true;
         popupComponent.showPopup(renderer,cssrenderer,controls,intersect.object.name);
       }
       if(intersect.object.name ==="Timetable"){
-        //window.open("https://www.erasmushogeschool.be/nl/faq");
         popupComponent.showPopup(renderer,cssrenderer,controls,intersect.object.name);
       }
       if(intersect.object.name ==="Music"){
