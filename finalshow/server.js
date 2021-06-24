4434c0c2d7b098e141b37a0277b66b0c31aa30d8
Timon Vanslambrouck <timon.vanslambrouck@student.ehb.be>
2021-06-24 21:23:52 +0200

    server

--- finalshow/server.js
+++ finalshow/server.js
@@ -4,19 +4,21 @@ const express = require('express');
 const app = express();
 
 function requireHTTPS(req, res, next) {
-    // The 'x-forwarded-proto' check is for Heroku
-    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
-        return res.redirect('https://' + req.get('host') + req.url);
-    }
-    next();
+  // The 'x-forwarded-proto' check is for Heroku
+  if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+    return res.redirect('https://' + req.get('host') + req.url);
+  }
+  next();
 }
 
 app.use(requireHTTPS);
 
 app.use(express.static('./dist/finalshow'));
 
-app.get('/*', function(req, res) {
-    res.sendFile('index.html', {root: 'dist/finalshow/'});
+app.get('/*', function (req, res) {
+  res.sendFile('index.html', {
+    root: 'dist/finalshow/'
   });
+});
 
 app.listen(process.env.PORT || 8080);
